ae19ee51cbb1afefc4851550c35dd23b4dafeb8b
chelsea.scheets@gmail.com <chelsea.scheets@gmail.com>
2016-08-25 20:39:31 -0600

    test

--- app.js
+++ app.js
@@ -1,73 +1,78 @@
 (function () {
-    
-    var items = ['Rock','Paper','Scissors'];
 
-    document.getElementById('btnRock').oneclick = playRock;
-    document.getElementById('btnPaper').oneclick = playPaper;
-    document.getElementById('btnScissors').oneclick = playScissors;
-    
-    var computerChoice = Math.floor((Math.random()* 3) + 1);
-    var computerItem = items [computerChoice - 1];
+    var items = ['Rock', 'Paper', 'Scissors'];
+    var computerItem;
 
+    document.getElementById('Rock').onclick = playRock;
+    document.getElementById('Paper').onclick = playPaper;
+    document.getElementById('Scissors').onclick = playScissors;
+    document.getElementById('PlayAgain').onclick = playAgain;
 
+    chooseComputerItem();
+
+    function playAgain() {
+        chooseComputerItem();
+        setCaption('Pick an Item!');
+    }
+
+    function chooseComputerItem() {
+        var computerChoice = Math.floor((Math.random() * 3) + 1);
+        computerItem = items[computerChoice - 1];
+        console.log('Computer Item: ' + computerItem);
+    }
     function playRock() {
-        play ('Rock');
+        play('Rock');
     }
-     function playPaper() {
-        play ('Paper');
+    function playPaper() {
+        play('Paper');
     }
-     function playScissors() {
-        play ('Scissors');
-
-    }
-    function play(myItem) {
-        setCaption("It's a Tie!");
+    function playScissors() {
+        play('Scissors');
     }
     function setCaption(caption) {
         document.getElementById('caption').innerHTML = caption;
     }
-    
-   function play() {
-        switch (computerItem) {
-	        case “Rock”:
-                if (myItem = "Rock") {
-                    setCaption("It's a tie!");
-                }
-                else if (myItem = "Paper") {
-                    setCaption("Computer wins!");
-                }
-                else (myItem = "scissors") {
-                    setCaption("Computer wins!");
-                }
-                break;
-            case “Paper”:
-                if (myItem) {
-                    setCaption("You win!");
-                }
-                else if (paper) {
-                    setCaption("It's a tie!");
-                }
-                else (scissors) {
-                    setCaption("Computer wins!");
-                }
-                break;
-            case “Scissors”:
-                if (rock) {
-                    setCaption("It's a tie!");
-                }
-                else if (paper) {
-                    setCaption("Computer wins!");
-                }
-                else (scissors) {
-                    setCaption("Computer wins!");
-                }
-                break;
-            case “”:
-                
-                break;
-        }
+    function showComputerItem() {
+        item = item || computerItem;
+        document.getElementById('computerItem').innerHTML = item;
     }
 
-
+    function play(myItem) {
+        switch (computerItem) {
+            case 'Rock':
+                if (myItem === 'Rock') {
+                    setCaption("Oh no, it's a tie!");
+                }
+                else if (myItem === 'Paper') {
+                    setCaption("You lose!");
+                }
+                else {
+                    setCaption("Wow, you kinda suck at this!");
+                }
+                break;
+            case 'Paper':
+                if (myItem === 'Rock') {
+                    setCaption("You win!");
+                }
+                else if (myItem === 'Paper') {
+                    setCaption("It's a tie!");
+                }
+                else {
+                    setCaption("You lose!");
+                }
+                break;
+            case 'Scissors':
+                if (myItem === 'Rock') {
+                    setCaption("You win!");
+                }
+                else if (myItem === 'Paper') {
+                    setCaption("Computer wins!");
+                }
+                else {
+                    setCaption("It's a tie!");
+                }
+                break;
+            }
+    }
 })();
 
