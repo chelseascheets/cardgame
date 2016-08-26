814d371adb0fff69ebb27314f474342af7095ae2
chelsea.scheets@gmail.com <chelsea.scheets@gmail.com>
2016-08-25 19:10:39 -0600

    stuff

---
+++ app.js
@@ -0,0 +1,73 @@
+(function () {
+    
+    var items = ['Rock','Paper','Scissors'];
+
+    document.getElementById('btnRock').oneclick = playRock;
+    document.getElementById('btnPaper').oneclick = playPaper;
+    document.getElementById('btnScissors').oneclick = playScissors;
+    
+    var computerChoice = Math.floor((Math.random()* 3) + 1);
+    var computerItem = items [computerChoice - 1];
+
+
+    function playRock() {
+        play ('Rock');
+    }
+     function playPaper() {
+        play ('Paper');
+    }
+     function playScissors() {
+        play ('Scissors');
+
+    }
+    function play(myItem) {
+        setCaption("It's a Tie!");
+    }
+    function setCaption(caption) {
+        document.getElementById('caption').innerHTML = caption;
+    }
+    
+   function play() {
+        switch (computerItem) {
+	        case “Rock”:
+                if (myItem = "Rock") {
+                    setCaption("It's a tie!");
+                }
+                else if (myItem = "Paper") {
+                    setCaption("Computer wins!");
+                }
+                else (myItem = "scissors") {
+                    setCaption("Computer wins!");
+                }
+                break;
+            case “Paper”:
+                if (myItem) {
+                    setCaption("You win!");
+                }
+                else if (paper) {
+                    setCaption("It's a tie!");
+                }
+                else (scissors) {
+                    setCaption("Computer wins!");
+                }
+                break;
+            case “Scissors”:
+                if (rock) {
+                    setCaption("It's a tie!");
+                }
+                else if (paper) {
+                    setCaption("Computer wins!");
+                }
+                else (scissors) {
+                    setCaption("Computer wins!");
+                }
+                break;
+            case “”:
+                
+                break;
+        }
+    }
+
+
+})();
+
